ee90c7598bd91710ed93b02bf7996a44a27961cb
Joseph Tongay <dirk.tongay@gmail.com>
2017-09-21 21:28:16 -0500

    added skelton and normalize for grid and normalize obvi

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import {
+  MdButtonModule,
+  MdCheckboxModule
+} from '@angular/material';
 
 @NgModule({
     imports: [MdButtonModule, MdCheckboxModule],
