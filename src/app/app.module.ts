df46bbdb17e6324f52c42bfad8e7b81c24439b74
Joseph Tongay <dirk.tongay@gmail.com>
2017-10-08 15:35:51 -0500

    i dont understand why angular material isnt loading inputs or forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { angularMaterialModule } from './modules/material.module'
+import { angularMaterialModule } from './modules/material.module';
+import { MaterialModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { routing } from './routes'
+import { routing } from './routes';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -17,6 +18,7 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { CollegeComponent } from './college/college.component';
 import { NflComponent } from './nfl/nfl.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
@@ -28,13 +30,15 @@ import { NflComponent } from './nfl/nfl.component';
     LoginComponent,
     SignupComponent,
     CollegeComponent,
-    NflComponent
+    NflComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     angularMaterialModule,
+    MaterialModule,
     BrowserAnimationsModule,
     routing
   ],
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,7 +2,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NflComponent } from './nfl/nfl.component';
 import { CollegeComponent } from './college/college.component';
-
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const APP_ROUTES: Routes = [
   {
@@ -16,6 +17,14 @@ const APP_ROUTES: Routes = [
   {
     path: 'college',
     component: CollegeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
   }
 ];
 
