627a74174b7a1a4fddb797d58882067fdb4bfe7d
Joseph Tongay <dirk.tongay@gmail.com>
2017-10-25 18:56:20 -0500

    started templating a bit with matchups that you havent picked yet

--- src/app/college-pick/college-pick.component.ts
+++ src/app/college-pick/college-pick.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { matchups } from '../matchups.sample';
 
 @Component({
   selector: 'app-college-pick',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollegePickComponent implements OnInit {
 
+  matchups: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.matchups = matchups;
   }
 
 }
---
+++ src/app/matchups.sample.ts
@@ -0,0 +1,487 @@
+export const matchups = {
+  schedule: [
+    {
+      away: {
+        rank: 20,
+        abbrev: "STAN",
+        record: "5-2",
+        team_name: "Cardinal",
+        team_location: [
+          "Stanford"
+        ]
+      },
+      home: {
+        rank: "unranked",
+        abbrev: "OREST",
+        record: "1-6",
+        team_name: "Beavers",
+        team_location: [
+          "Oregon",
+          "State"
+        ]
+      },
+      time: [
+        "9:00p",
+        "ETESPN"
+      ],
+      stadium: "Reser Stadium"
+    },
+    {
+      away: {
+        rank: "unranked",
+        abbrev: "TULANE",
+        record: "3-4",
+        team_name: "Wave",
+        team_location: [
+          "Tulane",
+          "Green"
+        ]
+      },
+      home: {
+        rank: 24,
+        abbrev: "MEM",
+        record: "6-1",
+        team_name: "Tigers",
+        team_location: [
+          "Memphis"
+        ]
+      },
+      time: [
+        "8:00p",
+        "ETCBSS"
+      ],
+      stadium: "Liberty Bowl Memorial"
+    },
+    {
+      away: {
+        rank: 5,
+        abbrev: "WIS",
+        record: "7-0",
+        team_name: "Badgers",
+        team_location: [
+          "Wisconsin"
+        ]
+      },
+      home: {
+        rank: "unranked",
+        abbrev: "ILL",
+        record: "2-5",
+        team_name: "Illini",
+        team_location: [
+          "Illinois",
+          "Fighting"
+        ]
+      },
+      time: [
+        "12:00p",
+        "ETESPN"
+      ],
+      stadium: "Memorial Stadium"
+    },
+    {
+      away: {
+        rank: 8,
+        abbrev: "MIA",
+        record: "6-0",
+        team_name: "Hurricanes",
+        team_location: [
+          "Miami",
+          "(FL)"
+        ]
+      },
+      home: {
+        rank: "unranked",
+        abbrev: "UNC",
+        record: "1-7",
+        team_name: "Heels",
+        team_location: [
+          "North",
+          "Carolina",
+          "Tar"
+        ]
+      },
+      time: [
+        "12:00p",
+        "ETESP2"
+      ],
+      stadium: "Kenan Memorial Stadium"
+    },
+    {
+      away: {
+        rank: 11,
+        abbrev: "OKST",
+        record: "6-1",
+        team_name: "Cowboys",
+        team_location: [
+          "Oklahoma",
+          "State"
+        ]
+      },
+      home: {
+        rank: 22,
+        abbrev: "WVU",
+        record: "5-2",
+        team_name: "Mountaineers",
+        team_location: [
+          "West",
+          "Virginia"
+        ]
+      },
+      time: [
+        "12:00p",
+        "ETABC"
+      ],
+      stadium: "Mountaineer Field at Milan Puskar Stadium"
+    },
+    {
+      away: {
+        rank: 2,
+        abbrev: "PSU",
+        record: "7-0",
+        team_name: "Lions",
+        team_location: [
+          "Penn",
+          "State",
+          "2Nittany"
+        ]
+      },
+      home: {
+        rank: 6,
+        abbrev: "OSU",
+        record: "6-1",
+        team_name: "Buckeyes",
+        team_location: [
+          "Ohio",
+          "State"
+        ]
+      },
+      time: [
+        "3:30p",
+        "ETFOX"
+      ],
+      stadium: "Ohio Stadium"
+    },
+    {
+      away: {
+        rank: 3,
+        abbrev: "UGA",
+        record: "7-0",
+        team_name: "Bulldogs",
+        team_location: [
+          "Georgia"
+        ]
+      },
+      home: {
+        rank: "unranked",
+        abbrev: "FLA",
+        record: "3-3",
+        team_name: "Gators",
+        team_location: [
+          "Florida"
+        ]
+      },
+      time: [
+        "3:30p",
+        "ETCBS"
+      ],
+      stadium: "EverBank Field"
+    },
+    {
+      away: {
+        rank: 4,
+        abbrev: "TCU",
+        record: "7-0",
+        team_name: "Frogs",
+        team_location: [
+          "TCU",
+          "4Horned"
+        ]
+      },
+      home: {
+        rank: 25,
+        abbrev: "IOWAST",
+        record: "5-2",
+        team_name: "Cyclones",
+        team_location: [
+          "Iowa",
+          "State"
+        ]
+      },
+      time: [
+        "3:30p",
+        "ETABC,",
+        "ESP2"
+      ],
+      stadium: "Jack Trice Stadium"
+    },
+    {
+      away: {
+        rank: 14,
+        abbrev: "NCST",
+        record: "6-1",
+        team_name: "Wolfpack",
+        team_location: [
+          "North",
+          "Carolina",
+          "State"
+        ]
+      },
+      home: {
+        rank: 9,
+        abbrev: "ND",
+        record: "6-1",
+        team_name: "Irish",
+        team_location: [
+          "Notre",
+          "Dame",
+          "9Fighting"
+        ]
+      },
+      time: [
+        "3:30p",
+        "ETNBC"
+      ],
+      stadium: "Notre Dame Stadium"
+    },
+    {
+      away: {
+        rank: "unranked",
+        abbrev: "UCLA",
+        record: "4-3",
+        team_name: "Bruins",
+        team_location: [
+          "UCLA"
+        ]
+      },
+      home: {
+        rank: 12,
+        abbrev: "WASH",
+        record: "6-1",
+        team_name: "Huskies",
+        team_location: [
+          "Washington"
+        ]
+      },
+      time: [
+        "3:30p",
+        "ETABC,",
+        "ESP2"
+      ],
+      stadium: "Alaska Airlines Field at Husky Stadium"
+    },
+    {
+      away: {
+        rank: 16,
+        abbrev: "MSU",
+        record: "6-1",
+        team_name: "Spartans",
+        team_location: [
+          "Michigan",
+          "State"
+        ]
+      },
+      home: {
+        rank: "unranked",
+        abbrev: "NU",
+        record: "4-3",
+        team_name: "Wildcats",
+        team_location: [
+          "Northwestern"
+        ]
+      },
+      time: [
+        "3:30p",
+        "ETESPN"
+      ],
+      stadium: "Ryan Field"
+    },
+    {
+      away: {
+        rank: "unranked",
+        abbrev: "HOU",
+        record: "4-3",
+        team_name: "Cougars",
+        team_location: [
+          "Houston"
+        ]
+      },
+      home: {
+        rank: 17,
+        abbrev: "USF",
+        record: "7-0",
+        team_name: "Bulls",
+        team_location: [
+          "South",
+          "Florida"
+        ]
+      },
+      time: [
+        "3:45p",
+        "ETESPU"
+      ],
+      stadium: "Raymond James Stadium"
+    },
+    {
+      away: {
+        rank: "unranked",
+        abbrev: "APSU",
+        record: "5-3",
+        team_name: "Governors",
+        team_location: [
+          "Austin",
+          "Peay"
+        ]
+      },
+      home: {
+        rank: 18,
+        abbrev: "UCF",
+        record: "6-0",
+        team_name: "Knights",
+        team_location: [
+          "UCF"
+        ]
+      },
+      time: [
+        "5:00p",
+        "ETESP3"
+      ],
+      stadium: "Spectrum Stadium"
+    },
+    {
+      away: {
+        rank: "unranked",
+        abbrev: "DUKE",
+        record: "4-4",
+        team_name: "Devils",
+        team_location: [
+          "Duke",
+          "Blue"
+        ]
+      },
+      home: {
+        rank: 13,
+        abbrev: "VT",
+        record: "6-1",
+        team_name: "Hokies",
+        team_location: [
+          "Virginia",
+          "Tech"
+        ]
+      },
+      time: [
+        "7:20p",
+        "ETACCN"
+      ],
+      stadium: "Lane Stadium/Worsham Field"
+    },
+    {
+      away: {
+        rank: "unranked",
+        abbrev: "GT",
+        record: "4-2",
+        team_name: "Jackets",
+        team_location: [
+          "Georgia",
+          "Tech",
+          "Yellow"
+        ]
+      },
+      home: {
+        rank: 7,
+        abbrev: "CLEM",
+        record: "6-1",
+        team_name: "Tigers",
+        team_location: [
+          "Clemson"
+        ]
+      },
+      time: [
+        "8:00p",
+        "ETABC,",
+        "ESP2"
+      ],
+      stadium: "Memorial Stadium"
+    },
+    {
+      away: {
+        rank: "unranked",
+        abbrev: "TEXTCH",
+        record: "4-3",
+        team_name: "Raiders",
+        team_location: [
+          "Texas",
+          "Tech",
+          "Red"
+        ]
+      },
+      home: {
+        rank: 10,
+        abbrev: "OU",
+        record: "6-1",
+        team_name: "Sooners",
+        team_location: [
+          "Oklahoma"
+        ]
+      },
+      time: [
+        "8:00p",
+        "ETABC,",
+        "ESP2"
+      ],
+      stadium: "Gaylord Family-Oklahoma Memorial Stadium"
+    },
+    {
+      away: {
+        rank: 15,
+        abbrev: "WSU",
+        record: "7-1",
+        team_name: "Cougars",
+        team_location: [
+          "Washington",
+          "State"
+        ]
+      },
+      home: {
+        rank: "unranked",
+        abbrev: "ARIZ",
+        record: "5-2",
+        team_name: "Wildcats",
+        team_location: [
+          "Arizona"
+        ]
+      },
+      time: [
+        "9:30p",
+        "ETPACN"
+      ],
+      stadium: "Arizona Stadium"
+    },
+    {
+      away: {
+        rank: 21,
+        abbrev: "USC",
+        record: "6-2",
+        team_name: "Trojans",
+        team_location: [
+          "USC"
+        ]
+      },
+      home: {
+        rank: "unranked",
+        abbrev: "ASU",
+        record: "4-3",
+        team_name: "Devils",
+        team_location: [
+          "Arizona",
+          "State",
+          "Sun"
+        ]
+      },
+      time: [
+        "10:45p",
+        "ETESPN"
+      ],
+      stadium: "Sun Devil Stadium, Frank Kush Field"
+    }
+  ]
+}
