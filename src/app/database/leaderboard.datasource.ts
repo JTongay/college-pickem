6f20a8c360409edeaacba96f68d112c8ffa69216
Joseph Tongay <dirk.tongay@gmail.com>
2017-11-29 19:51:37 -0600

    cleaning up unused crap

--- src/app/database/leaderboard.datasource.ts
+++ src/app/database/leaderboard.datasource.ts
@@ -7,21 +7,15 @@ import {ScoringService} from '../scoring.service';
 
 export class LeaderboardDatasource extends DataSource<any> {
 
-  baseUrl = 'https://football-picks-api.herokuapp.com/api';
-  devUrl = 'http://localhost:3000/api';
-
   constructor(
     private scoringService: ScoringService
   ) {
     super();
-    this.baseUrl = 'https://football-picks-api.herokuapp.com/api';
-    this.devUrl = 'http://localhost:3000/api';
   }
 
 
   /** Connect function called by the table to retrieve one stream containing the data to render. */
   connect(): Observable<any> {
-    console.log('connected?');
     return this.scoringService.getLeaderBoard(1, 1);
   }
   disconnect() {}
