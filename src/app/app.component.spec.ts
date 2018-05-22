5b798532edc9ce9a340c49df07a16988e543ea4e
Joseph Tongay <dirk.tongay@gmail.com>
2018-05-21 21:40:03 -0500

    chipping away at these unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {AuthService} from './auth.service';
+import {DebugElement, ElementRef} from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MainComponent,
+        NavbarComponent,
+        FooterComponent
       ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        HttpModule
+      ],
+      providers: [
+        AuthService
+      ]
     }).compileComponents();
   }));
 
@@ -16,17 +33,23 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  it('should render the footer component on load', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    const app: any = fixture.debugElement.nativeElement;
+    expect(app.querySelector('app-footer')).toBeTruthy();
   }));
+  it('should render the navbar component on load', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app: any = fixture.debugElement.nativeElement;
+    expect(app.querySelector('app-navbar')).toBeTruthy();
+  }));
+  it('should render the main container component on load', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app: any = fixture.debugElement.nativeElement;
+    expect(app.querySelector('app-main')).toBeTruthy();
+  }));
+
 });
---
+++ src/app/validators/date.spec.ts
@@ -0,0 +1,16 @@
+import {FormControl} from '@angular/forms';
+import {async} from '@angular/core/testing';
+import { DateValidator } from './date';
+
+fdescribe('DateValidator', () => {
+  it('should pass validation', async(() => {
+    const validator: DateValidator = new DateValidator();
+    const testValue = validator.date(new FormControl('11221989'));
+    expect(testValue).toEqual({date: true});
+  }));
+  it('should fail validation with any letters', async(() => {
+    const validator: DateValidator = new DateValidator();
+    const testValue = validator.date(new FormControl('11221b'));
+    expect(testValue).toBeNull();
+  }));
+});
